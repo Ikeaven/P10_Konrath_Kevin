90177a20549a6486df3b6c332c9c9b2e31804272
Konrath <36306272+Ikeaven@users.noreply.github.com>
2021-11-28 10:59:04 +0100

    user manager ok

--- SoftDesk_API/authentication/models.py
+++ SoftDesk_API/authentication/models.py
@@ -2,32 +2,16 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import BaseUserManager
 
-
-# Create your models here.
-# class UserManager(BaseUserManager):
-#     def create_user(self, email, password=None, first_name=None, last_name=None, **extra_fields):
-#         if not email:
-#             raise ValueError('Enter an email address')
-#         if not first_name:
-#             raise ValueError('Enter a first name')
-#         if not last_name:
-#             raise ValueError('Enter a last name')
-#         email = self.normalize_email(email)
-#         user = self.model(email=email, first_name=first_name, last_name=last_name, **extra_fields)
-#         user.set_password(password)
-#         user.save(using=self._db)
-#         return user
-
-#     def create_superuser(self, email, password, first_name, last_name):
-#         user = self.create_user(email, password=password, first_name=first_name, last_name=last_name)
-#         user.is_superuser = True
-#         user.is_staff = True
-#         user.save(using=self._db)
-#         return user
+from authentication.managers import CustomUserManager
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     username = None
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name', 'password']
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
