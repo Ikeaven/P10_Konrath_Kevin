21ca9860a31b7794d1d0cd0b2d6a788c568e8daf
Konrath <36306272+Ikeaven@users.noreply.github.com>
2021-11-29 08:43:41 +0100

    signup

--- SoftDesk_API/authentication/views.py
+++ SoftDesk_API/authentication/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import authenticate, login
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.utils.translation import gettext_lazy as _
 
 from .models import User
 from .serializers import UserSerializer
@@ -21,25 +22,27 @@ def signup(request):
     if request.method == 'POST':
         # TODO vérifier la présence de tous les éléments ci dessous :
         first_name = request.POST.get('first_name')
+        if not first_name:
+            raise ValueError(_('first_name must be set'))
+
         last_name = request.POST.get('last_name')
+        if not last_name:
+            raise ValueError(_('last_name must be set'))
         email = request.POST.get('email')
         password = request.POST.get('password')
 
 
     user = User.objects.filter(email=email)
-    print('1')
     if not user.exists():
-        print('2')
-        # le user n'existe pas on peut le créer
         user = User.objects.create(
+            email=email,
             first_name=first_name,
             last_name=last_name,
             password=make_password(password, 'salt', 'default')
         )
-        print('TEST')
         user = authenticate(email=request.POST.get('email'), password=request.POST.get('password'))
         if user is not None:
             login(request, user)
-            return Response({"message": "Hello, world!"})
+            return Response({"message": _(f'user {first_name} {last_name} created')})
     else:
-        return Response({"message":"le user n'a pas pu être créé"})
+        return Response({"message":_("le user n'a pas pu être créé")})
